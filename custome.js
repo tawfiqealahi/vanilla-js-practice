107ad089cd4edc7d5e65cc15e8873633f2fd8146
tawfiqealahi <tawfiqealahi99@gmail.com>
2024-06-20 10:15:19 +0600

    first commit

---
+++ custome.js
@@ -0,0 +1,34 @@
+function pract(a,b) {
+   let a= 'tawfiq';
+   let b='e alahi';
+   (+b
+   
+)};
+
+   console.log(pract());
+
+   
+//    let newLi = document.createElement('li');
+//     newLi.textContent = 'products';
+//     menu.appendChild(newLi);
+
+//     newLi= document.createElement('li')
+//     newLi.textContent = 'about';
+
+//     const menu = document.querySelector('#menu');
+//     menu.appendChild(newLi);
+    
+
+
+
+//     let element = document.createElement("div");
+//     element.id = 'container';
+//     element.className = 'newElement';
+    
+//     let text = document.createTextNode('tawfiq e alahi is a good boy');
+//     element.appendChild(text);
+//     element.innerHTML = ' <h1>javascript element</h1> <p> new element here it is written by me </p>';
+//     document.body.appendChild(element);
+
+    
+
